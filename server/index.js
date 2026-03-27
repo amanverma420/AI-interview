f545bdfeef23513a6b29c04dd56235a27f7e31c3
Aman Verma <72482906+amanverma420@users.noreply.github.com>
2026-03-28 00:26:47 +0530

    fix cors final

--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
+app.options("*", cors());
 import dotenv from "dotenv"
 import connectDb from "./config/connectDb.js"
 import cookieParser from "cookie-parser"
 dotenv.config()
-import cors from "cors"
+// import cors from "cors"
 import authRouter from "./routes/auth.route.js"
 import userRouter from "./routes/user.route.js"
 import interviewRouter from "./routes/interview.route.js"
 import paymentRouter from "./routes/payment.route.js"
 
 const app = express()
+const cors = require("cors");
+
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-}))
+  origin: [
+    "http://localhost:5173",
+    "https://ai-interview-psi-six.vercel.app"
+  ],
+  credentials: true
+}));
 
 
 app.use(express.json())
